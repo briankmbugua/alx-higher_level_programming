3b8c0831698d1e004c3584215765b3798cbde4ea
briankmbugua <briankmbuguak@gmail.com>
2023-03-13 08:49:22 +0300

    using node process.argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argumnt found');
+} else {
+  console.log('Arguments found');
+}
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -2,3 +2,5 @@
 Write a script that prints "Javascript is amazing"
 ## 1. 3 languages
 A script that prints 3 lines:
+## 2. Arguments
+A script that prints a message depending on the number arguments passed
