4c697c26c60b00c5f6c230bce3f42b2fed5517f9
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 15:19:41 +0300

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] > 0 || isNaN(process.argv[2])) {
-  if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-    console.log('Missing size');
-  } else {
-    let square = '';
-    for (let i = 0; i < process.argv[2]; i++) {
-      for (let j = 0; j < process.argv[2]; j++) {
-        square += 'X';
-      }
-      square += '\n';
-    }
-
-    console.log(square);
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
