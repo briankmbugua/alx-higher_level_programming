07126e9bbeff84bd7a8eb6e684a8fcec9957e8d7
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 13:38:22 +0300

    corrections for 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-if (argv.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
