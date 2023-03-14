62a675bc57a330e92620333daa83b6fd991e4ec5
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 13:39:59 +0300

    corrections for 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv[2] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  console.log(process.argv[2]);
 }
