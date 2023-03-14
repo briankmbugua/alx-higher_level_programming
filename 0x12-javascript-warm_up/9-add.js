281679c4dfa2790cf9be8f1289ba0cd324552218
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 19:34:40 +0300

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  if (isNaN(a) || isNaN(b)) {
+    return console.log(NaN);
+  }
+  console.log(parseInt(a) + parseInt(b));
 }
-
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+add(process.argv[2], process.argv[3]);
