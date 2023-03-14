fad715a3e5bb97f58b5ed94c1ab4b1a7466752fb
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 13:43:59 +0300

    corrections for 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-if (!Number.isInteger(argv[2])) {
-  if (isNaN(parseFloat(argv[2]))) {
+if (!Number.isInteger(process.argv[2])) {
+  if (isNaN(parseFloat(process.argv[2]))) {
     console.log('Not a number');
   } else {
-    console.log(`My number: ${parseInt(argv[2])}`);
+    console.log(`My number: ${parseInt(process.argv[2])}`);
   }
 }
