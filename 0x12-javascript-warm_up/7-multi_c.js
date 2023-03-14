953aab13244943b8006109c77578b0a61076a8d8
briankmbugua <briankmbuguak@gmail.com>
2023-03-14 14:10:58 +0300

    corrections for 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  let x = process.argv[2];
+  while (x > 0) {
+    console.log('C is fun');
+    x--;
+  }
+}
