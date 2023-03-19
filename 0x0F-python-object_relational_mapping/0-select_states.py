c63ec8782392a66e01879d9f2e035826f7422f0c
briankmbugua <briankmbuguak@gmail.com>
2023-03-19 22:54:20 +0300

    python object relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,11 @@
 Module that has a scripts that lists all states from
 the database hbtn_0e_0_usa
 """
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    """
-    Execute only if the file is run directly
-    """
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
