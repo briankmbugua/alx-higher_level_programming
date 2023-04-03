124c6f68b638693292adf9f31fb280d9d922a3ee
briankmbugua <briankmbuguak@gmail.com>
2023-04-04 00:51:33 +0300

    find peak in a list of unsorted intergers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    
+    low = 0
+    high = len(list_of_integers) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        
+        if list_of_integers[mid] < list_of_integers[mid+1]:
+            low = mid + 1
+        else:
+            high = mid
+    
+    return list_of_integers[low]
