df023be1482970d4ba9660b9d101be9ca46e3f71
briankmbugua <briankmbuguak@gmail.com>
2023-04-03 16:59:27 +0300

    return size of the response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl the first argument passed to the script using $1 variable and uses word count to return the size of the response
-curl -s "$1" | grep -i content-length
+curl -s "$1" | wc -c
