f1a2bab8905ca54d9e98dd8a25adbd1244a6baad
briankmbugua <briankmbuguak@gmail.com>
2023-04-28 16:22:56 +0300

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], (error, data) => {
-  if (error) console, log(error);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
+  }
 });
