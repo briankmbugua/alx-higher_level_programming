df27077cc13fd5aa7178e6d9095c741701cf4ba2
briankmbugua <briankmbuguak@gmail.com>
2023-04-28 19:00:15 +0300

    count starwars character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2],(error, response, body) => {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
