b1af92f1c34bc742763c20b7860761a373b88aa7
briankmbugua <briankmbuguak@gmail.com>
2023-05-07 23:53:28 +0300

    change text when an element is clicked

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('update_header').click(function () {
+  $('#update_header').click(function () {
     const header = $('header');
     header.text('New text content');
   });
