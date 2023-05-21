f0a9dabfc3bab2f76475c7ae36c9cd71cda8cef1
briankmbugua <briankmbuguak@gmail.com>
2023-05-21 19:33:48 +0300

    create a database a user and grant the user select privilages

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- script that creates database hbtn_0d_2 and the user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
