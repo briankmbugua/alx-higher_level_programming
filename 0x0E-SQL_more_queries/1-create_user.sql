e85159cf474f93318d02c1e2636d4edcd1ce7ae9
briankmbugua <briankmbuguak@gmail.com>
2023-05-21 19:21:59 +0300

    create user and grant them all privilages

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- create a user on MySQL server and grant them all the privilages of the root user.user_0d_1 password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
