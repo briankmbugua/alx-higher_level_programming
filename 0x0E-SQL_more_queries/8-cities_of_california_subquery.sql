1c68cc89807c839e745560b561b994acc757a56d
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 14:50:17 +0300

    SELECT all cities in the state table where name is California the result set should be sorted by cities.id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- script that lists all the cities of california that can be found in the database hbtn_0d_usa
 SELECT cities FROM states
-WHERE name = california
+WHERE name = California
 SORT BY cities.id
