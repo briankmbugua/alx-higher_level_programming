ffbbf5f3392d61ab51e16a66594d8ba7d069aa75
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 17:55:51 +0300

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,7 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked display tv_shows.title, tv_show_genres.genre_id order in ASC by tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
