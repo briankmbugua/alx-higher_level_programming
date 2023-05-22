87efb26e8ff187ee90a635ec44ca39397e8fb88a
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 17:00:42 +0300

    list and order

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- list all shows contained in the database hbtn_0d_tvshows display tv_shows.title-tv_show_genres.genre_id sort in ASC by tv_shows.title and tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
