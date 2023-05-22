2a93a2a3b633023692e10ecee6420349f8b184f2
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 14:08:16 +0300

    created a database a table with a primary key and a foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT,
     PRIMARY KEY(id),
-    state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
