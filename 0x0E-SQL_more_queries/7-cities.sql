6c638df92fdadb1788480f5fb8d4670c86d4038f
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 14:00:27 +0300

    created a database a table with a primary key and a foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,4 @@
+-- script that create the database hbtn_0d_usa and table cities in the same database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
