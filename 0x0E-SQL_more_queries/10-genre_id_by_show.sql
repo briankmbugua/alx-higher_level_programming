21d3c0d8b7a456fcd4d2602ff83ff93418496a04
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 16:46:21 +0300

    imported a database dump file and used ORDER BY and ASC

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- list all shows contained in hbtn_0d_tvshwos
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
