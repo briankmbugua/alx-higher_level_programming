c09fca1e4d0c13124e66c012fa7fa18e66943f2a
briankmbugua <briankmbuguak@gmail.com>
2023-05-22 17:05:12 +0300

    list and order using left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- list all shows contained in the database hbtn_0d_tvshows display tv_shows.title-tv_show_genres.genre_id sort in ASC by tv_shows.title and tv_shows_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
