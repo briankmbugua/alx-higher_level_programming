6b54b20590178965682b122707752ba14aba864a
briankmbugua <briankmbuguak@gmail.com>
2023-05-29 22:39:54 +0300

    added model_city

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -22,7 +22,6 @@ class State(Base):
     class attribute name that represents a column
      of a string with maximum 128 characters and
       can't be null
-
     """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
